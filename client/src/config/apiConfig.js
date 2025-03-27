a9bc5a9bf1f8167dd88f1e72c37c03a069f4b598
Niran B G S H <127426411+HasinduNiran@users.noreply.github.com>
2025-03-27 17:49:01 +0530

    edit . add modal create extrnl

--- client/src/config/apiConfig.js
+++ client/src/config/apiConfig.js
@@ -3,7 +3,13 @@ const API_CONFIG = {
   BASE_URL: "http://localhost:3000", // Base URL for your backend
   ENDPOINTS: {
     PROMOTIONS: "/api/promotions",
-    INVENTORY: "/api/inventory", // Added inventory endpoint
+    INVENTORY: {
+      BASE: "/api/inventory",
+      RETRIEVED: {
+        ALL: "/api/inventory/retrieved/all",
+        SINGLE: (id) => `/api/inventory/retrieved/${id}`
+      }
+    },
   },
 };
 
